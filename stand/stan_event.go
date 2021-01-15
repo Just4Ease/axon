38c3e72f126d6d97284b381fce8583c0a526af64
Justice Nefe <justicenefe@gmail.com>
2021-01-15 02:12:30 +0100

    feat: Updated stan to use direct nats connection in it's streaming client conf.
    - Request & Reply use nats conn
    - Pub, Sub uses stan conn

--- stand/stan_event.go
+++ stand/stan_event.go
@@ -2,6 +2,7 @@ package stand
 
 import (
 	"github.com/Just4Ease/axon"
+	"github.com/nats-io/nats.go"
 	"github.com/nats-io/stan.go"
 )
 
@@ -23,6 +24,29 @@ func (s stanEvent) Topic() string {
 
 func newEvent(msg *stan.Msg) axon.Event {
 	return &stanEvent{
+		m: msg,
+	}
+}
+
+
+type natsEvent struct {
+	m *nats.Msg
+}
+
+func (n natsEvent) Ack() {
+	return
+}
+
+func (n natsEvent) Data() []byte {
+	return n.m.Data
+}
+
+func (n natsEvent) Topic() string {
+	return n.m.Subject
+}
+
+func newNatsEvent(msg *nats.Msg) axon.Event {
+	return &natsEvent{
 		m: msg,
 	}
 }
--- stand/stand.go
+++ stand/stand.go
@@ -6,24 +6,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Just4Ease/axon"
+	"github.com/nats-io/nats.go"
 	"github.com/nats-io/stan.go"
 	"log"
 	"strings"
+	"time"
 )
 
 type natsStore struct {
-	client      stan.Conn
+	stanClient  stan.Conn
+	natsClient  *nats.Conn
 	serviceName string
 }
 
 func (s *natsStore) Publish(topic string, message []byte) error {
-	return s.client.Publish(topic, message)
+	return s.stanClient.Publish(topic, message)
 }
 
 func (s *natsStore) Subscribe(topic string, handler axon.SubscriptionHandler) error {
 	errChan := make(chan error)
 	go func(errChan chan<- error) {
-		_, err := s.client.QueueSubscribe(topic, s.serviceName, func(msg *stan.Msg) {
+		_, err := s.stanClient.QueueSubscribe(topic, s.serviceName, func(msg *stan.Msg) {
 			event := newEvent(msg)
 			go handler(event)
 		}, stan.DurableName(s.serviceName), stan.SetManualAckMode())
@@ -38,86 +41,46 @@ func (s *natsStore) Subscribe(topic string, handler axon.SubscriptionHandler) er
 }
 
 func (s *natsStore) Request(requestURI string, payload []byte, v interface{}) error {
-	errChan := make(chan error)
-	eventChan := make(chan axon.Event)
+	nc := s.natsClient
+
 	req := axon.NewRequestPayload(requestURI, payload)
-	go func(errChan chan<- error, eventChan chan<- axon.Event, req *axon.RequestPayload, conn stan.Conn) {
-		doneSig := make(chan bool)
-		sub, err := s.client.QueueSubscribe(req.ReplyPipe, s.serviceName, func(msg *stan.Msg) {
-			event := newEvent(msg)
-			eventChan <- event
-			doneSig <- true
-		}, stan.DurableName(s.serviceName), stan.SetManualAckMode())
-		if err != nil {
-			errChan <- err
-		}
-
-		defer func(errChan chan<- error) {
-			if err := sub.Unsubscribe(); err != nil {
-				errChan <- err
-			}
-		}(errChan)
-
-		ok := <-doneSig
-		if ok {
-			return
-		}
-	}(errChan, eventChan, req, s.client)
-
-	go func(errChan chan<- error, payload *axon.RequestPayload, topic string) {
-		data, err := payload.Compact()
-		if err != nil {
-			log.Printf("failed to compact request of: %s for transfer with the following errors: %v", topic, err)
-			errChan <- err
-			return
-		}
-
-		if err := s.Publish(topic, data); err != nil {
-			log.Printf("failed to send request for: %s with the following errors: %v", topic, err)
-			errChan <- err
-		}
-	}(errChan, req, requestURI)
-	// Read address from
-	for {
-		select {
-		case err := <-errChan:
-			log.Print("failed to receive reply-response with the following errors: ", err)
-			return err
-		default:
-			event := <-eventChan
-
-			// This is the ReplyPayload
-			var reply axon.ReplyPayload
-			if err := json.Unmarshal(event.Data(), &reply); err != nil {
-				log.Print("failed to unmarshal reply event into reply struct with the following errors: ", err)
-				event.Ack()
-				return err
-			}
-
-			// Check if reply has an issue.
-			if replyErr := reply.GetError(); replyErr != nil {
-				event.Ack()
-				return replyErr
-			}
-
-			// Unpack Reply's payload.
-			if err := json.Unmarshal(reply.GetPayload(), v); err != nil {
-				log.Print("failed to unmarshal reply payload into struct with the following errors: ", err)
-				event.Ack()
-				return err
-			}
-
-			event.Ack()
-			return nil
-		}
+	data, err := req.Compact()
+	msg, err := nc.Request(requestURI, data, time.Second*1)
+	if err != nil {
+		log.Print("Error making eventful request: ", err)
+		return err
 	}
+
+	event := newNatsEvent(msg)
+	var reply axon.ReplyPayload
+	if err := json.Unmarshal(event.Data(), &reply); err != nil {
+		log.Print("failed to unmarshal reply event into reply struct with the following errors: ", err)
+		event.Ack()
+		return err
+	}
+
+	// Check if reply has an issue.
+	if replyErr := reply.GetError(); replyErr != nil {
+		event.Ack()
+		return replyErr
+	}
+
+	// Unpack Reply's payload.
+	if err := json.Unmarshal(reply.GetPayload(), v); err != nil {
+		log.Print("failed to unmarshal reply payload into struct with the following errors: ", err)
+		event.Ack()
+		return err
+	}
+
+	event.Ack()
+	return nil
 }
 
 func (s *natsStore) Reply(topic string, handler axon.ReplyHandler) error {
 	errChan := make(chan error)
 	go func(errChan chan<- error) {
-		_, err := s.client.QueueSubscribe(topic, s.serviceName, func(msg *stan.Msg) {
-			event := newEvent(msg)
+		_, err := s.natsClient.QueueSubscribe(topic, s.serviceName, func(msg *nats.Msg) {
+			event := newNatsEvent(msg)
 			var reqPl axon.RequestPayload
 			decoder := json.NewDecoder(bytes.NewBuffer(event.Data()))
 			decoder.UseNumber()
@@ -135,13 +98,11 @@ func (s *natsStore) Reply(topic string, handler axon.ReplyHandler) error {
 				return
 			}
 
-			if err := s.Publish(reqPl.GetReplyAddress(), data); err != nil {
+			if err := msg.Respond(data); err != nil {
 				log.Print("failed to reply data to the incoming request with the following error: ", err)
 				return
 			}
-
-			event.Ack()
-		}, stan.DurableName(s.serviceName), stan.SetManualAckMode())
+		})
 		if err != nil {
 			errChan <- err
 		}
@@ -168,16 +129,22 @@ func Init(opts axon.Options, clusterId string, options ...stan.Option) (axon.Eve
 	//	clientOptions.Authentication = pulsar.NewAuthenticationToken(opts.AuthenticationToken)
 	//}
 
+	nc, err := nats.Connect(opts.Address, nats.Name(name))
+	if err != nil {
+		return nil, err
+	}
+
 	var optsList []stan.Option
-	optsList = append(optsList, stan.NatsURL(opts.Address))
+	optsList = append(optsList, stan.NatsConn(nc))
 	optsList = append(optsList, options...)
-	st, err := stan.Connect(clusterId, name, optsList...)
+	st, err := stan.Connect(clusterId, fmt.Sprintf("%s-%s", name, axon.GenerateRandomString()), optsList...)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect with NATS with the provided configuration. failed with error: %v", err)
 	}
 
 	return &natsStore{
-		client:      st,
+		stanClient:  st,
+		natsClient:  nc,
 		serviceName: name,
 	}, nil
 }
